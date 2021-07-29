ce538517834612a55c9f20f3aabde3fb6040f02b
Anandkv10 <87757981+Anandkv10@users.noreply.github.com>
2021-07-29 13:29:04 +0530

    Add files via upload

---
+++ Javascript/Ex6.js
@@ -0,0 +1,8 @@
+ /**
+ * 
+ */
+
+const fruits=["banana","orange","apple","mango"];
+document.getElementById("demo1").innerHTML=fruits;
+fruits.shift();
+document.getElementById("demo2").innerHTML=fruits;
